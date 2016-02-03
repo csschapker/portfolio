d37841e9992e91df34c7c4e4cdaf40b45cf70814
Scott Schapker <csschapker94@gmail.com>
2016-02-03 00:24:58 -0500

    Add theme colors and some basic content to the home page.

--- app/components/tabs/tabBarDirective.js
+++ app/components/tabs/tabBarDirective.js
@@ -3,7 +3,7 @@ app.directive('tabBar', [
         return {
             restrict: 'E',
             transclude: true,
-            template: '<nav class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect"><div ng-transclude class="mdl-tabs__tab-bar"></div></nav>',
+            template: '<nav class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect mdl-shadow--2dp"><div ng-transclude class="mdl-tabs__tab-bar"></div></nav>',
             replace: true
         };
     }
