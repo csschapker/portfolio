13cfb1f71217b8a76cc71e94d39f30c10a07bf48
Scott Schapker <csschapker94@gmail.com>
2016-03-15 11:53:32 -0400

    Created site footer.

---
+++ app/components/site-footer/siteFooterDirective.js
@@ -0,0 +1,9 @@
+app.directive('siteFooter', [
+    function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'app/components/site-footer/siteFooterView.html'
+        }
+    }
+]);
