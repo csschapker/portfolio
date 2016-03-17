c584009dc1e3302cc169151bfae92b90e683eeee
Scott Schapker <csschapker94@gmail.com>
2016-03-16 20:50:25 -0400

    Added notifications for sending messages.

--- app/components/contact-form/contactFormDirective.js
+++ app/components/contact-form/contactFormDirective.js
@@ -1,53 +1,52 @@
 app.directive('contactForm', [
-    'Api',
-    function(Api) {
+    'Api', 'Notification',
+    function(Api, Notification) {
         return {
             restrict: 'E',
             replace: true,
             templateUrl: 'app/components/contact-form/contactFormView.html',
             link: function(scope, element, attrs) {
-                scope.submitted = false;
-                scope.badName = true;
-                scope.badEmail = true;
-                scope.badMessage = true;
-
                 scope.data = {};
 
                 scope.submitForm = function() {
                     scope.submitted = true;
-                    if (formIsValid()) { sendMessage(); }
+
+                    if (formIsValid()) {
+                        sendMessage();
+                    } else {
+                        Notification.error('Make sure you fill out all of the fields to send a message!');
+                    }
                 };
 
                 function checkName() {
-                    scope.badName = (!scope.data.name || !scope.data.name.length);
+                    return (scope.data.name && scope.data.name.length);
                 }
 
                 function checkEmail() {
-                    var regex = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/igm;
-                    var emailIsValid = regex.test(scope.data.email);
-                    scope.badEmail = (!scope.data.email || !scope.data.email.length || !emailIsValid);
+                    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return regex.test(scope.data.email);
                 }
 
                 function checkMessage() {
-                    scope.badMessage = (!scope.data.message || !scope.data.message.length);
+                    return (scope.data.message && scope.data.message.length);
                 }
 
                 function formIsValid() {
-                    checkName();
-                    checkEmail();
-                    checkMessage();
-                    return (!scope.badName && !scope.badEmail && !scope.badMessage);
+                    var nameIsValid = checkName();
+                    var emailIsValid = checkEmail();
+                    var messageIsValid = checkMessage();
+                    return (nameIsValid && emailIsValid && messageIsValid);
                 }
 
                 function sendMessage() {
                     Api.sendPersonalMessage(scope.data,
-                    function(response) {
+                    function() {
                         scope.data = {};
                         scope.submitted = false;
-                        console.log(response);
+                        Notification.primary('Message sent! Thanks!');
                     },
-                    function(response) {
-                        console.log(response);
+                    function() {
+                        Notification.error('Oh no, Your message could not be sent! Try again later or try following my links to contact me.');
                     });
                 }
             }
