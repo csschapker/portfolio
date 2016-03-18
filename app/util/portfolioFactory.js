7bf0606968a588ac6b6e5b7322997034a3b75a87
Scott Schapker <csschapker94@gmail.com>
2016-03-18 11:01:54 -0400

    Added Perryville and changed blue logo.

--- app/util/portfolioFactory.js
+++ app/util/portfolioFactory.js
@@ -52,7 +52,7 @@ app.factory('Portfolio', [
                 'pokemon/pokemonDB-badges.png',
                 'pokemon/pokemonDB-search.png'
             ],
-            thumb: 'pokemon/pokemonDB-home.png'
+            thumb: 'pokemon/pokemonDB-thumb.png'
         };
 
         var ipr = {
@@ -137,12 +137,64 @@ app.factory('Portfolio', [
             thumb: 'careercenter/careercenter-thumb.png'
         };
 
+        var perryville = {
+            name: 'Perryville Battlefield',
+            types: [
+                'Android',
+                'iOS'
+            ],
+            description: '...',
+            role: '...',
+            technologies: [
+                'Java',
+                'Objective-C'
+            ],
+            images: [
+                'perryville/perryville-ios-splash.png',
+                'perryville/perryville-android-mappage.png',
+                'perryville/perryville-ios-optionslist.png',
+                'perryville/perryville-ios-welcome.png',
+                'perryville/perryville-ios-troops.png',
+                'perryville/perryville-android-troopspage.png',
+                'perryville/perryville-ios-stoplist.png',
+                'perryville/perryville-ios-stopscreen.png',
+                'perryville/perryville-ios-site.png',
+                'perryville/perryville-ios-gallery.png',
+                'perryville/perryville-ios-quote.png',
+                'perryville/perryville-android-quotepage.png',
+                'perryville/perryville-ios-credits.png'
+            ],
+            thumb: 'perryville/perryville-thumb.png'
+        };
+
+        var simulator = {
+            name: 'Soccer Camp Simulator',
+            types: [
+                'Web'
+            ],
+            description: '...',
+            role: '...',
+            technologies: [
+                'AngularJS',
+                '...'
+            ],
+            images: [
+                'simulator/simulator-home.png',
+                'simulator/simulator-intro.png',
+                'simulator/simulator-questions.png',
+                'simulator/simulator-order.png'
+            ],
+            thumb: 'simulator/simulator-thumb.png'
+        };
+
 
         projects.push(scholarship);
         projects.push(bsuMap);
         projects.push(ipr);
         projects.push(pokemon);
         projects.push(careerCenter);
+        projects.push(perryville);
+        projects.push(simulator);
 
         return {
             getProjects: function() { return angular.copy(projects); }
