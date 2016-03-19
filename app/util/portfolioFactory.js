c01f8a67eff6f9cadcef78dda0c4d1407443d6c0
Scott Schapker <csschapker94@gmail.com>
2016-03-19 06:25:31 -0400

    Added project content!

--- app/util/portfolioFactory.js
+++ app/util/portfolioFactory.js
@@ -8,12 +8,10 @@ app.factory('Portfolio', [
             types: [
                 'Web'
             ],
-            description: 'The Ball State Digital Corps began offering a scholarship to incoming freshman in order to work for the Digital Corps and kickstart their professional portfolio. The scholarship was created to filter applicants based on skill and personality. The application is a survey future students to ensure cultural and academic compatibility. We created a website for potential candidates to use to apply for the scholarship. Our team achieved success using a PHP backend built on the Slim framework, tying into our MySQL database and a frontend driven by AngularJS, Sass and Gulp.',
-            role: 'I did things. Theses are the really cool things I did. Well they were kinda cool.',
+            description: 'The Digital Corps started offering a scholarship for incoming Ball State freshmen in 2016, and an application was needed. This internal project was started to build a website to receive responses from applicants and for staff to view applications in order to select a winner for the scholarship. The project also required a front end that projected the atmosphere and direction of the Digital Corps and a backend to help retrieve and store information user information and activity. The system’s database was built from scratch, storing things like user responses from the application, application reviews by staff members, questions/prompts for the application and user credentials.',
+            role: 'My work on the application was mostly on the front end. I translated much of what you see from our designer’s concept to the appropriate HTML and CSS to match the design language. Using AngularJS and the popular AngularUI Router frameworks, I built the flow of the application. I think it’s necessary to mention that most of my work on the front end was at a high level, building the views and setting up the majority of the styles in a way that allowed for others to add the more detailed stuff later. Additionally, I created our initial Gulp tasks to allow us to use Sass and to just speed up development a little. On the back end, my work was limited to mostly changing a couple of endpoints for HTTP requests and altering a few lines of PHP logic for those requests.',
             technologies: [
-                'AngularJS',
-                'PHP',
-                '...'
+                'HTML', 'AngularJS', 'AngularUI Router', 'Gulp', 'Sass', 'PHP', 'Slim', 'MySQL', 'PHPStorm', 'phpMyAdmin'
             ],
             images: [
                 'scholarship/scholarship-register.png',
@@ -32,16 +30,14 @@ app.factory('Portfolio', [
         };
 
         var pokemon = {
-            name: 'Pokemon DB Clone',
+            name: 'Pokémon DB Clone',
             types: [
                 'Web'
             ],
-            description: 'Ahhh a description.',
-            role: 'I did things. Theses are the really cool things I did. Well they were kinda cool.',
+            description: 'In a database design class at Ball State in 2015, my professor asked us to pick an application that we would want to build throughout the semester and would required a database. So naturally, my group went with Pokémon; we came to this decision not just because we’re cool, but because we knew there is already as much information available about Pokémon as we could want to we wouldn’t have to worry about coming up with mock data. The final product we ended up with is a simple website using AngularJS and PHP that allows users to browse through different information about the original Pokémon.',
+            role: 'For this assignment, unlike most of my other projects, I spent most of my time working on the back end and almost none on the front end. The database itself was the focus of the course, so my group and I spent most of our time working on the and decided to use MySQL because of how popular it is. I built out most of the backend and decided to use PHP and the Slim framework because it allowed me to jump quickly into building HTTP request endpoints.',
             technologies: [
-                'AngularJS',
-                'PHP',
-                '...'
+                'HTML', 'AngularJS', 'ngRoute', 'PHP', 'Slim', 'MySQL', 'PHPStorm', 'phpMyAdmin', 'MySQLWorkbench'
             ],
             images: [
                 'pokemon/pokemonDB-home.png',
@@ -60,12 +56,10 @@ app.factory('Portfolio', [
             types: [
                 'iOS'
             ],
-            description: '...',
-            role: '...',
+            description: 'Indiana Public Radio at Ball State asked the Digital Corps to build some mobile applications for the station. The basic idea for these applications was to simply recreate the IPR website in a native application form for Android and iOS. The applications feature a live stream of the station so users can easily listen from their devices. Users can also set notifications for shows so they can get a reminder to tune in. Additionally, the applications provide news stories with audio clips that are posted on the IPR website and access to some recorded shows and segments.',
+            role: 'This project was the first time I really took on an iOS project without much significant help. That being said, I learned most of my iOS development skills while creating this application. I set up a media player for the IPR live stream and integrated it with other media players for underwriter messages, news clips and recorded shows. I built local notifications for users to get alerts about shows they wanted to listen to. Finally, I created settings to switch the station stream between low and high quality, to automatically stop the stream after a certain number of minutes and to allow the user to choose whether or not to start the stream automatically.',
             technologies: [
-                'Objective-C',
-                'Xcode',
-                '...'
+                'Objective-C', 'iOS', 'Xcode'
             ],
             images: [
                 'ipr/ipr-loading.png',
@@ -88,12 +82,10 @@ app.factory('Portfolio', [
             types: [
                 'Android'
             ],
-            description: '...',
-            role: '...',
+            description: 'Ball State University has a fairly large campus and it’s easy to lost, especially if you’re unfamiliar with the area. Well a few years ago, the Digital Corps launched a mobile application for iOS and Android to help make getting around Ball State easier. The BSU Campus Map showed all of the buildings on campus and some information about each one. Well by the summer of 2015 the applications were very outdated, sometimes struggling to run as the systems they were designed on were being dropped. So we started a project to redesign the applications and revamp the system as whole with more accurate and more useful information. The apps now show the entities closest to the user’s location and more detailed information about each building. When a user views a building, they can get directions to it and find out what offices, dining areas and labs are in it. Users can also the normal hours of operation for most places on campus. There are also indicators of what is in a building and where restrooms are located. Additionally, users can find parking lots and meters, landmarks and ATMs.',
+            role: 'This project is one my favorites from my time at the Digital Corps because it’s used daily by many students around Ball State’s campus, and I have used it often myself. I really enjoyed working on the Android application because it has a smooth interface that looks clean and crisp. We built all of the views in the application from scratch and used some interesting Android libraries for material design. Overall, I spent most of my time working on interactions with map, such as showing and hiding the appropriate markers at the appropriate times and getting the proper animations out of the map. I also worked a lot on with search bar and getting it act correctly with suggested searches based on all of the entities on campus and search terms that we provided.',
             technologies: [
-                'Java',
-                'Android Studio',
-                '...'
+                'Java', 'Android', 'Gradle', 'Android Studio'
             ],
             images: [
                 'bsumap/bsumap-homepage.png',
@@ -117,12 +109,10 @@ app.factory('Portfolio', [
             types: [
                 'Web'
             ],
-            description: '...',
-            role: '...',
+            description: 'The Ball State Career Center wanted a kiosk-type application that allowed students to look at information about careers, find careers they might enjoy and connect with the Career Center. The system we built uses data from the O*NET occupation database and allows users to take a couple routes for finding job information. First, they can browse through a simple list of occupations, sorting by salary or name, and save any they might find interesting. Another option the user has is to answer a short survey, picking two of their values or personality types that they find most important. The results of the survey are used to present the user with a list of careers that they might be interested in and can also save for later. The last part of the application allows users to enter an email address to receive the information about careers they saved and additional information from the Career Center.',
+            role: 'Most of my work on the kiosk application was on the front end. I used Gulp to automate some tasks for me and Sass for styling. I built most of the application with AngularJS and AngularUI Router so I could easily handle all of the logic for each state separately. Additionally, I added Angular Material to this project to add some subtle animation and aesthetics. My work on the back end, which was built with CakePHP, was limited to creating the email template and functionality.',
             technologies: [
-                'AngularJS',
-                'Sass',
-                '...'
+                'HTML', 'AngularJS', 'AngularUI Router', 'Angular Material', 'Gulp', 'Sass', 'PHP', 'CakePHP', 'MySQL', 'PHPStorm'
             ],
             images: [
                 'careercenter/careercenter-loading.png',
@@ -172,11 +162,10 @@ app.factory('Portfolio', [
             types: [
                 'Web'
             ],
-            description: '...',
-            role: '...',
+            description: 'A Sports Law instructor at Ball State came to the Digital Corps with an idea for an interactive assignment for her students in the summer of 2016. This turned into one of the most interesting and fun projects I have worked on. The idea was to make a web-based assignment that didn’t just make students answer questions (although they still get to, of course), but made them pull from their knowledge and apply it to the real world. The solution was to ask the students to plan for a simulated youth soccer camp to see if, given a certain budget, they would hire the appropriate amount of staff as well as equipment. The students would need to pick out the items and personnel they needed and distribute them among the different fields of the soccer camp. After doing so, they would be presented a few prompts about things that could potentially go wrong and answer how they would respond to those situations.',
+            role: 'With this project, my work has been evenly distributed between the front end and back end. On the front end, I set up the site as an AngularJS application and successfully integrated jQuery and jQueryUI with it. I made a few Gulp tasks so we could get going with Sass, and I added AngularUI Router to control the flow of the application. Additionally, I built out a drag-and-drop system, merging jQuery with AngularJS (which was not an easy task), to be used to distribute items between fields. My back-end work has been fairly basic. I created a simple tables in our MySQL database and a small PHP backend with the Slim framework.',
             technologies: [
-                'AngularJS',
-                '...'
+                'HTML', 'AngularJS', 'AngularUI Router', 'jQuery', 'jQueryUI', 'Gulp', 'Sass', 'PHP', 'Slim', 'MySQL', 'PHPStorm', 'phpMyAdmin'
             ],
             images: [
                 'simulator/simulator-home.png',
@@ -184,7 +173,8 @@ app.factory('Portfolio', [
                 'simulator/simulator-questions.png',
                 'simulator/simulator-order.png'
             ],
-            thumb: 'simulator/simulator-thumb.png'
+            thumb: 'simulator/simulator-thumb.png',
+            inProgress: true
         };
 
 
@@ -193,7 +183,7 @@ app.factory('Portfolio', [
         projects.push(ipr);
         projects.push(pokemon);
         projects.push(careerCenter);
-        projects.push(perryville);
+        //projects.push(perryville);
         projects.push(simulator);
 
         return {
